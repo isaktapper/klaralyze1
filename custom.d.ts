29fb018d31f2a15792b0e8dbd0a51a495b3c8f40
Isak Tapper <isaktapper@MacBook-Air-som-tillhor-Isak.local>
2025-04-06 02:20:25 +0200

    Remove all Zod dependencies and references

--- custom.d.ts
+++ custom.d.ts
@@ -1,7 +1,5 @@
 declare module 'next/link';
 declare module 'next/navigation';
 declare module 'react-hook-form';
-declare module '@hookform/resolvers/zod';
-declare module 'zod';
 declare module 'sonner';
 declare module 'framer-motion'; 
