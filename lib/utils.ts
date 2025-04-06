75c3b9061f8ce9d716ce0956d1ff0748f0008ea1
Isak Tapper <isak.tapper@gmail.com>
2025-04-06 18:24:54 +0200

    Simplify utils.ts to avoid bundling issues

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+type ClassValue = string | number | boolean | undefined | null | { [key: string]: any };
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return inputs
+    .flat()
+    .filter(Boolean)
+    .map(input => {
+      if (typeof input === 'string') return input;
+      if (input && typeof input === 'object') {
+        return Object.entries(input)
+          .filter(([, value]) => Boolean(value))
+          .map(([key]) => key)
+          .join(' ');
+      }
+      return '';
+    })
+    .join(' ')
+    .trim();
 }
